faf8dac6676fa0f7daf33d56f7ad81a51c5cda6a
Nijin P S <nijingithub@gmail.com>
2022-10-27 16:09:58 +0530

    Added airline and flight models

---
+++ src/models/flight.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const flightSchema = new mongoose.Schema({
+    departureAirport : {
+        type : String,
+        required : true
+    },
+    arrivalAirport : {
+        type : String,
+        required : true
+    },
+    duration : {
+        type : Number,
+        required : true
+    },
+    airline : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        ref : 'Airline'
+    },
+    flightDate: {
+        type : Date,
+        default : Date.now
+    },
+    departureTime: {
+        type : Timestamp
+    },
+    arrivalTime: {
+        type : Timestamp
+    }
+}
+)
+
+const flightModel = new mongoose.model('Flight', flightSchema);
+
+module.exports = flightModel;
